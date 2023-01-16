b8e569407df624ddc54622c0ab78a69086af5fa9
Lucie.HJ.Roh <92660068+Lucie-HJ-ROH@users.noreply.github.com>
2023-01-16 16:18:11 +0000

    Change Save

--- MidtermTravel/MainWindow.xaml.cs
+++ MidtermTravel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MidtermTravel
 {
@@ -23,6 +24,89 @@ namespace MidtermTravel
         public MainWindow()
         {
             InitializeComponent();
+            try
+            {
+                Globals.dbContext = new TripDBContext();
+                LvTrip.ItemsSource = Globals.dbContext.Trips.ToList();
+             }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(this, "Error reading from database\n" + ex.Message, "Fatal error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                // Close();
+                Environment.Exit(1);
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string destination = DestinationInput.Text;
+            string name = NameInput.Text;
+            string passport = PassportInput.Text;
+            DateTime departureDate = DepartureDate.SelectedDate.Value;
+            DateTime returnDate = ReturnDate.SelectedDate.Value;
+            try
+            {
+                // public Trip(string destination, string name, string passport, DateTime departureDate, DateTime returnDate)
+                Trip newTrip = new Trip(destination, name, passport, departureDate, returnDate);
+                Globals.dbContext.Trips.Add(newTrip);
+                Globals.dbContext.SaveChanges();
+
+                LvTrip.ItemsSource = Globals.dbContext.Trips.ToList();
+
+            }
+            catch (ArgumentException ex) {
+                MessageBox.Show(this, ex.Message, "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show(this, "Error reading from database\n" + ex.Message, "Database error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            ResetFields();
+        }
+        private void ResetFields()
+        {
+            DestinationInput.Text = "";
+            NameInput.Text = "";
+            PassportInput.Text = "";
+            DepartureDate.SelectedDate = DateTime.Today;
+            ReturnDate.SelectedDate = DateTime.Today;
+
+        }
+
+        private void LvTrip_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Trip currSelTrip = LvTrip.SelectedItem as Trip;
+            BtnDelete.IsEnabled = (currSelTrip != null);
+            BtnUpdate.IsEnabled = (currSelTrip != null);
+
+            if(currSelTrip!= null)
+            {
+                DestinationInput.Text = currSelTrip.Destination;
+                NameInput.Text = currSelTrip.TravlerName;
+                PassportInput.Text = currSelTrip.TravlerPassport;
+                DepartureDate.SelectedDate = currSelTrip.DepartureDate;
+                ReturnDate.SelectedDate = currSelTrip.ReturnDate;
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Trip currSelTrip = LvTrip.SelectedItem as Trip;
+            if(currSelTrip== null) { return; }
+
+
+            var result = MessageBox.Show(this, "Are you sure you want to delete this item?\n" + currSelTrip, "Confirm deletion", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result != MessageBoxResult.Yes) { return; }
+
+            Globals.dbContext.Trips.Remove(currSelTrip);
+            Globals.dbContext.SaveChanges();
+
+            LvTrip.ItemsSource = Globals.dbContext.Trips.ToList();
+            LvTrip.Items.Refresh();
+            ResetFields();
+
         }
     }
 }
--- MidtermTravel/Trip.cs
+++ MidtermTravel/Trip.cs
@@ -1,12 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MidtermTravel
 {
-    internal class Trip
+    public class Trip
     {
+        public Trip() { }
+        public Trip(string destination, string name, string passport, DateTime departureDate, DateTime returnDate)
+        {
+            Destination = destination;
+            TravlerName = name;
+            TravlerPassport= passport;
+            DepartureDate = departureDate;
+            ReturnDate = returnDate;
+
+
+        }
+
+        public int Id { get; set; }
+
+
+        //Destination
+        private string _destination;
+
+        [Required]
+        [StringLength(30)]
+        public string Destination
+        {
+            get
+            {
+                return _destination;
+            }
+            set
+            {          
+                if (value.Length < 2 || value.Length > 31)
+                {
+                    throw new ArgumentException("Destination length must be 2-30 characters long");
+                }
+                _destination = value;
+            }
+        }
+
+
+        //TravlersName
+        private string _travlerName;
+
+        [Required]
+        [StringLength(30)]
+        public string TravlerName
+        {
+            get
+            {
+                return _travlerName;
+            }
+            set
+            {
+                if (value.Length < 2 || value.Length > 31)
+                {
+                    throw new ArgumentException("Travler's name length must be 2-30 characters long");
+                }
+                _travlerName = value;
+            }
+        }
+
+        //TravlersPassport
+        private string _travlerPassport;
+
+        [Required]
+        [StringLength(30)]
+        public string TravlerPassport
+        {
+            get
+            {
+                return _travlerPassport;
+            }
+            set
+            {
+                /*
+                if (Regex.IsMatch(_travlerPassport, @"^([a-zA-Z0-9])+$"))
+                {
+                    throw new ArgumentException("Travler's passport must be started  with 2 upper caracters.");
+                }*/
+                if (value.Length < 2 || value.Length > 31)
+                {
+                    throw new ArgumentException("Travler's passport length must be 2-30 characters long");
+                }
+                _travlerPassport = value;
+            }
+        }
+
+        //Departure date
+        private DateTime _departureDate;
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        // [DisplayFormat(ApplyFormatInEditMode = true)] //, DataFormatString = "{yyyy/MM/0:dd}")]
+        public DateTime DepartureDate
+        {
+            get
+            {
+                return _departureDate;
+            }
+            set
+            {
+                if (value.Year < 1900 || value.Year > 2099)
+                {
+                    throw new ArgumentException("Invalid year. Must be between 1900-2099.");
+                }
+                _departureDate = value;
+            }
+        }
+
+        //Return date
+        private DateTime _returnDate;
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        // [DisplayFormat(ApplyFormatInEditMode = true)] //, DataFormatString = "{yyyy/MM/0:dd}")]
+        public DateTime ReturnDate
+        {
+            get
+            {
+                return _returnDate;
+            }
+            set
+            {
+                if ((value.Year < 1900 || value.Year > 2099) &&( _departureDate.Year > value.Year && _departureDate.Month > value.Month && _departureDate.Date > value.Date))
+                {
+                    throw new ArgumentException("Invalid year. Must be between 1900-2099.");
+                }
+                _returnDate = value;
+            }
+        }
+
+
     }
 }
